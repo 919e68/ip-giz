0caef8e65dafeb7f331b966cbfcfc454c5f96deb
Wilson Anciro <konekred@gmail.com>
2019-12-03 22:05:22 +0800

    add ping function

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const isValid = require('./lib/is-valid')
 const increment = require('./lib/increment')
 const iterate = require('./lib/iterate')
+const ping = require('./lib/ping')
 
 module.exports = {
   isValid,
+  ping,
   increment,
   iterate
 }
