e3e554c119d427716147546ff53b2c4e476dcd8e
Wilson Anciro <konekred@gmail.com>
2019-12-03 21:49:34 +0800

    remove test files

--- tests/test.js
+++
@@ -1,7 +0,0 @@
-const isValidIP = require('../lib/is-valid')
-const increment = require('../lib/increment')
-const iterate = require('../lib/iterate')
-
-iterate('192.168.1.1', '192.168.1.244', (ip) => {
-  console.log(ip)
-}, true)
